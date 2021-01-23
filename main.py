b6701cc992b5ead8a71b9922758757ccda172048
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-01-23 21:22:04 +0300

    Modify __init__ method of VKBot.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #! usr/bin/env python3
 # -*- coding: utf8 -*-
 
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+from key import group_key
+from id import group_id
+
+
 class VKBot:
-    def __init__(self):
-        pass
+    def __init__(self, group_id=None, group_access_key=None):
+        self._id = group_id
+        self._token = group_access_key
 
 
 if __name__ == '__main__':
