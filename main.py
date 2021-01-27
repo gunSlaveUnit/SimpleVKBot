8814c8486f5d7e852925d296f86abfbd04b4c4e2
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-01-27 11:04:49 +0300

    Change import statement.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # -*- coding: utf8 -*-
 
 from VKBot import VKBot
-import settings
+try:
+    import settings
+except ImportError:
+    exit('Rename settings.py.default.py to settings.py and set ID and TOKEN')
 
 if __name__ == '__main__':
     bot = VKBot(group_id=settings.ID, group_access_key=settings.TOKEN)
