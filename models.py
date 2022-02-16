9e582bc36417ae64aba613eb48ac1934021968de
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-16 21:42:01 +0300

    Added exception when there is no settings file

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 #! usr/bin/env python3
 # -*- coding: utf8 -*-
+
 from pony.orm import Database, Required, Json
 
-import settings
+try:
+    import settings
+except ImportError:
+    exit('Rename settings.py.default.py to settings.py and set ID and TOKEN')
 
 db = Database()
 db.bind(**settings.DB_CONFIG)
